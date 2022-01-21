a83443af92a63bdf3f22b9357a1bf572451ae0b4
Sorina <sorina.grigorescu98@gmail.com>
2022-01-21 14:25:20 +0200

    sorina s work

--- src/pages/FindBooks.js
+++ src/pages/FindBooks.js
@@ -7,17 +7,25 @@ import { Formik } from 'formik';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
+import Chip from '@mui/material/Chip';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function sleep(delay = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 }
-function FindBooks(props) {
+
+function AutocompleteInput(props) {
   const [open, setOpen] = useState(false);
   const [options, setOptions] = useState([]);
   const loading = open && options.length === 0;
+
+  //const [value, setValue] = React.useState('');
+  // const [inputValue, setInputValue] = React.useState('');
+
+  const userInput = [];
 
   useEffect(() => {
     let active = true;
@@ -26,13 +34,17 @@ function FindBooks(props) {
       return undefined;
     }
 
-    (async () => {
-      await sleep(1e3); // For demo purposes.
-
-      if (active) {
-        setOptions([...topFilms]);
-      }
-    })();
+    axios
+      .get('https://localhost:44392/api/' + props.apiName)
+      .then((result) => {
+        console.log(result);
+        if (active) {
+          setOptions([...result.data]);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
 
     return () => {
       active = false;
@@ -44,6 +56,72 @@ function FindBooks(props) {
       setOptions([]);
     }
   }, [open]);
+
+  let inputChips = userInput
+    .map(data =>
+      <Chip label={data.label} variant="outlined" />
+    );
+
+  return <div>
+    <Autocomplete
+      id={props.label}
+      sx={{ width: 300 }}
+
+      //value={value}
+      onChange={(event, newValue) => {
+        console.log("before: " + JSON.stringify(userInput))
+        userInput.push({...newValue})
+        console.log("after: " + JSON.stringify(userInput))
+      }}
+      // inputValue={inputValue}
+      // onInputChange={(event, newInputValue) => {
+      //   console.log("new input value: " + newInputValue)
+      //   setInputValue(newInputValue);
+      // }}
+
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      isOptionEqualToValue={(option, value) =>
+        option.id === value.id
+      }
+      getOptionLabel={(option) => {
+        if (props.apiName === "books") 
+          return option.title
+        else
+          return option.name
+      }}
+      options={options}
+      loading={loading}
+      style={{ width: '100%' }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={props.label}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? (
+                  <CircularProgress color='inherit' size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+    {inputChips}
+  </div>;
+}
+
+function FindBooks(props) {
+
   return (
     <div className='w-full flex flex-row px-80'>
       <div className='w-1/2 flex items-center min-h-screen md:flex'>
@@ -98,115 +176,13 @@ function FindBooks(props) {
           }) => (
             <form className='w-full mb-1' onSubmit={handleSubmit}>
               <div className='form-control w-full'>
-                <Autocomplete
-                  id='asynchronous-demo'
-                  sx={{ width: 300 }}
-                  open={open}
-                  onOpen={() => {
-                    setOpen(true);
-                  }}
-                  onClose={() => {
-                    setOpen(false);
-                  }}
-                  isOptionEqualToValue={(option, value) =>
-                    option.title === value.title
-                  }
-                  getOptionLabel={(option) => option.title}
-                  options={options}
-                  loading={loading}
-                  style={{ width: '100%' }}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label='Carte'
-                      InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                          <>
-                            {loading ? (
-                              <CircularProgress color='inherit' size={20} />
-                            ) : null}
-                            {params.InputProps.endAdornment}
-                          </>
-                        ),
-                      }}
-                    />
-                  )}
-                />
+                <AutocompleteInput label='Carte' apiName='books'/>
               </div>
               <div className='form-control w-full mt-4'>
-                <Autocomplete
-                  id='asynchronous-demo'
-                  sx={{ width: 300 }}
-                  open={open}
-                  onOpen={() => {
-                    setOpen(true);
-                  }}
-                  onClose={() => {
-                    setOpen(false);
-                  }}
-                  isOptionEqualToValue={(option, value) =>
-                    option.title === value.title
-                  }
-                  getOptionLabel={(option) => option.title}
-                  options={options}
-                  loading={loading}
-                  style={{ width: '100%' }}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label='Autor'
-                      InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                          <>
-                            {loading ? (
-                              <CircularProgress color='inherit' size={20} />
-                            ) : null}
-                            {params.InputProps.endAdornment}
-                          </>
-                        ),
-                      }}
-                    />
-                  )}
-                />
+                <AutocompleteInput label='Autor' apiName='authors'/>
               </div>
               <div className='form-control w-full mt-4'>
-                <Autocomplete
-                  id='asynchronous-demo'
-                  sx={{ width: 300 }}
-                  open={open}
-                  onOpen={() => {
-                    setOpen(true);
-                  }}
-                  onClose={() => {
-                    setOpen(false);
-                  }}
-                  isOptionEqualToValue={(option, value) =>
-                    option.title === value.title
-                  }
-                  getOptionLabel={(option) => option.title}
-                  options={options}
-                  loading={loading}
-                  style={{ width: '100%' }}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label='Gen'
-                      InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                          <>
-                            {loading ? (
-                              <CircularProgress color='inherit' size={20} />
-                            ) : null}
-                            {params.InputProps.endAdornment}
-                          </>
-                        ),
-                      }}
-                    />
-                  )}
-                />
+                <AutocompleteInput label='Gen' apiName='categories'/>
               </div>
               <div className='flex space-x-1 w-full mt-8 mb-8'>
                 <button type='submit' className='btn btn-primary w-full mr-4'>
@@ -223,53 +199,3 @@ function FindBooks(props) {
 
 export default FindBooks;
 
-// Top films as rated by IMDb users. http://www.imdb.com/chart/top
-const topFilms = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-];
